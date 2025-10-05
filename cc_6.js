966fc85d077185708692729afc361cba6ad1a566
Wyatt Mullen <mullenw@usf.edu>
2025-10-05 13:02:20 -0400

    add and push employee

--- cc_6.js
+++ cc_6.js
@@ -16,4 +16,20 @@ class Manager extends Employee {
     return `${this.name} manages the ${this.department} department and leads a team of ${this.teamSize} employees.`;
   }
 }
+class Company {
+  constructor() {
+    this.employees = [];
+  }
+
+  addEmployee(employee) {
+    this.employees.push(employee);
+  }
+
+  listEmployees() {
+    console.log("---- Company Employees ----");
+    this.employees.forEach(employee => {
+      console.log(employee.describe());
+    });
+  }
+}
 
