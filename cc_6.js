a5f8884a18bfeea92197ab3984e96d27ec2f925d
Wyatt Mullen <mullenw@usf.edu>
2025-10-05 12:32:51 -0400

    set up class

--- cc_6.js
+++ cc_6.js
@@ -1,5 +1,19 @@
-class Employee{name, department}
-    constructor (name, department);{
+class Employee {
+    constructor(name, department) {
         this.name = name;
         this.department = department;
     }
+}
+describe() {
+    return `${this.name} works in ${this.department}`;
+}
+class Manager extends Employee {
+    constructor(name, department, teamSize) {
+        super(name, department);
+        this.teamSize = teamSize;
+    }
+     describe() {
+    return `${this.name} manages the ${this.department} department and leads a team of ${this.teamSize} employees.`;
+  }
+}
+
